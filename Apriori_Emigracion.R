c560b64832a5d790a4ce7e1a95b6a6669e6ea115
201503440 <201503440@users.noreply.github.com>
2024-10-24 12:39:09 -0600

    Update Apriori_Emigracion.R

--- Apriori_Emigracion.R
+++ Apriori_Emigracion.R
@@ -1,7 +1,7 @@
 library(arules)
 library(dplyr)
 
-data <- read.csv('C:\\Users\\65826\\Documents\\U\\Censo2018\\db_csv_\\MIGRACION_BDP.csv', sep = ",")
+data <- read.csv('/home/cesarsazo/Documentos/R/db_csv_/MIGRACION_BDP.csv', sep = ",")
 
 reglas <- apriori(data, parameter = list(support=0.2, confidence=0.5))
 
